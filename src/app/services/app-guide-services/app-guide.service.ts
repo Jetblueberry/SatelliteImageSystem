834ecd4a52df3f20bce7239161857708cc9a5e6b
Julian1412 <phananhle2002@gmail.com>
2024-04-27 11:51:12 +0700

    Update help done

--- src/app/services/app-guide-services/app-guide.service.ts
+++ src/app/services/app-guide-services/app-guide.service.ts
@@ -56,14 +56,52 @@ export class AppGuideService {
       message: 'Enter a location name, the map will drive you to that address by locating a point ',
       key: 'cf3',
       header: 'Location search',
-      icon: 'fa-solid fa-location-dot',
+      icon: 'fa-solid fa-tree-city',
       acceptLabel: 'Next',
       acceptIcon: 'pi pi-arrow-right',
       accept: () => {
         this.displayLocationDialog = false;
+        this.confirmDialogGuide4();
       },
       reject: () => {
         this.displayLocationDialog = false;
+      }
+    });
+  }
+
+  confirmDialogGuide4() {
+    this.displayMapSettingDialog = true;
+    this.confirmationService.confirm({
+      message: 'Choose your map types you want',
+      key: 'cf4',
+      header: 'Map settings',
+      icon: 'fa-brands fa-canadian-maple-leaf',
+      acceptLabel: 'Next',
+      acceptIcon: 'pi pi-arrow-right',
+      accept: () => {
+        this.displayMapSettingDialog = false;
+        this.confirmDialogGuide5();
+      },
+      reject: () => {
+        this.displayMapSettingDialog = false;
+      }
+    });
+  }
+
+  confirmDialogGuide5() {
+    this.displayPrintScreenshotDialog = true;
+    this.confirmationService.confirm({
+      message: 'Take a photo of the map and it will be downloaded to your device. Beside this, you can print the map on types you want',
+      key: 'cf5',
+      header: 'Screenshot and print',
+      icon: 'fa-solid fa-camera',
+      acceptLabel: 'Finish',
+      acceptIcon: 'pi',
+      accept: () => {
+        this.displayPrintScreenshotDialog = false;
+      },
+      reject: () => {
+        this.displayPrintScreenshotDialog = false;
       }
     });
   }
